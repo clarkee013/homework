054deecc56afb9aeb4f28b793d52333d844fd561
David Clarkson <clarkee013@gmail.com>
2017-05-22 16:33:22 +0100

    started sports_team homework task

---
+++ sports_team.rb
@@ -0,0 +1,14 @@
+class Students
+
+def initialize(input_student_name, input_student_cohort, input_student_prog_language)
+  @student_name = input_student_name
+  @student_cohort = input_student_cohort
+  @student_prog_language = input_student_prog_language
+end
+
+
+
+
+
+
+end # END of CLASS
---
+++ sports_team_spec.rb
@@ -0,0 +1,12 @@
+require ('minitest/autorun')
+require ('minitest/rg')
+require_relative ('sports_team')
+
+class TestSportsTeam < MiniTest::Test
+
+  def setup ()
+
+set up sports team here
+  end
+
+end # END of CLASS
